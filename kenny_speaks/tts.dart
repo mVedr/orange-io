033604d9e60b70d39ee2b72e7e15a31a7b139af8
Ved Recharla <94962003+mVedr@users.noreply.github.com>
2024-02-08 11:03:51 +0530

    Update tts.dart

--- kenny_speaks/tts.dart
+++ kenny_speaks/tts.dart
@@ -1,17 +1,19 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
+/* Singleton Text-To-Speech Instance */
 class TextToSpeech{
+  //TTS Object
   static FlutterTts tts = FlutterTts();
+  //Function for initializing tts object
   static initTTS(){
     tts.setLanguage("en-US");
     tts.setPitch(1.2);
     tts.setSpeechRate(0.7);
     tts.setVolume(1);
-
   }
+  //Function for speaking
   static speak(String text) async{
     tts.setStartHandler((){
-
     });
     await tts.awaitSpeakCompletion(true);
     tts.speak(text);
