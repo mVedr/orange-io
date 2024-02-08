ab0b094d2539c42d2dcc31c28738a47e4fa9596d
Ved Recharla <94962003+mVedr@users.noreply.github.com>
2024-02-08 10:51:56 +0530

    Update main3.dart

--- img_gen/main3.dart
+++ img_gen/main3.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:contact_state_app/img_gen/homescreen.dart';
 
-//void main(){
- // runApp(MyApp());
-//}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
